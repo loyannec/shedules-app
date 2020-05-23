17534138271a30eb1f5e4952d58188a91ea5cd44
Loyanne Cristine <loyanne.cristine@gmail.com>
2020-05-23 13:47:34 +0100

    refactored for to use the new class DatabaseService

--- database.js
+++ database.js
@@ -1,40 +1,8 @@
-var mysql = require("mysql");
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    port: 3306
-});
-var databaseName = "schedules_db";
-var tableName = "schedules";
+var DatabaseService = require('./DatabaseService');
+var service = new DatabaseService();
 
-connection.connect();
+service.connect();
 
-connection.query(`DROP DATABASE if exists ${databaseName}`, function (error,results,fields) {
-    if (error) throw error;
-});
+service.setUp();
 
-connection.query(`CREATE DATABASE ${databaseName}`, function (error,results,fields) {
-    if (error) throw error;
-});
-
-connection.query(`USE ${databaseName}`, function (error,results,fields) {
-    if (error) throw error;
-});
-
-connection.query(`CREATE TABLE ${tableName} (`+
-                    "id INT PRIMARY KEY NOT NULL AUTO_INCREMENT," +
-                    "username VARCHAR (50) UNIQUE NOT NULL," +
-                    "day_of_the_week INTEGER NOT NULL," +
-                    "start_time TIME NOT NULL," +
-                    "end_time TIME NOT NULL" +
-                    ")", function (error,results,fields) {
-    if (error) throw error;
-});
-
-connection.query("SHOW DATABASES", function (error,results,fields) {
-    if (error) throw error;
-    console.log("The databases are: ", results);
-});
-
-connection.end();
+service.disconnect();
