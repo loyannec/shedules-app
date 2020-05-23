a2f3d55c5f8bd08e0395d025edb4148e133ea6e8
Loyanne Cristine <loyanne.cristine@gmail.com>
2020-05-23 13:45:24 +0100

    inserted and recovered schedules in the routes

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const DatabaseService = require('./DatabaseService');
 const express = require('express');
 const exphbs  = require('express-handlebars');
 
@@ -9,12 +10,22 @@ app.set('view engine', 'handlebars');
 
 app.use(express.urlencoded());
 
-app.get('/', (req, res) => res.render('home'));
+app.get('/', (req, res) => {
+    var database = new DatabaseService();
+    database.connect();
+    database.retrieveSchedules((schedules) => {
+        database.disconnect();
+        res.render('home', { schedules: schedules });
+    });
+});
 app.get('/new', (req, res) => res.render('form'));
 
 app.post('/new', (req, res) => {
+    var database = new DatabaseService();
+    database.connect();
+    database.insertSchedule(req.body);
+    database.disconnect();
     res.render('home');
 });
-
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
